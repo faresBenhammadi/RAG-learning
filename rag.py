35ec53034d63beb1096a97c3e5a11f3f8360c023
faresBenhammadi <mohammed-fares.benhammadi@ensia.edu.dz>
2026-08-18 16:59:27 +0100

    first rag built

--- rag.py
+++ rag.py
@@ -4,6 +4,11 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+load_dotenv()
+
+
+## LOAD DATA
 
 def load_documents(docs_data):
     if not  os.path.exists(docs_data):
@@ -36,15 +41,33 @@ def chunking(documents,chunk_size = 800, chunk_overlap = 0):
     chunks = text_splitter.split_documents(documents)
     for i,chunk in enumerate(chunks[:5]):
         print(f"--- chunk {i+1} ---")
-        print(f"source : {chunk.metadata["source"]}")
+        print(f"source : {chunk.metadata['source']}")
         print(f"length : {len(chunk.page_content)} characters")
         print(chunk.page_content)
     return chunks
 
 
+def vector_store(chunks,dir = "db/chroma_db"):
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="BAAI/bge-small-en-v1.5"
+    )
+    print("--- creating vector database ---")
+    vector_store = Chroma(
+        persist_directory=dir,
+        embedding_function=embedding_model,
+        collection_metadata={"hnsw:space":"cosine"}
+    )
+
+    vector_store.add_documents(chunks)
+    print("--- finished creating vector database ---")
+
+    return vector_store
+
+
 def main():
     documents = load_documents(docs_data="docs")
     chunks = chunking(documents=documents)
+    vector_store(chunks=chunks)
 
     
 
